1e9bf7c7d0458d7818a2759aff04e0ec6ced2459
maxkonar <kamil_karasiewicz@op.pl>
2023-12-18 15:08:22 +0100

    change version

--- ws-price-history-for-woocommerce.php
+++ ws-price-history-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name:       WS Price History For Woocommerce
  * Text Domain:       ws_price_history
  * Description:       Add price history for products
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 6.0
  * Author:            Web Systems
  * Author URI:        https://www.web-systems.pl/
